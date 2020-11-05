ade69da2b73e039204657572424a443663ed26a1
CHENJUN <chenjun156@pingan.com.cn>
2020-11-05 21:18:28 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# tmall-table
-天猫卖人气表格统计
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.less';
+
+const Index = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+};
+
+export default Index;
